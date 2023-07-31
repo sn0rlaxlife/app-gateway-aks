6597cf65067b664cfcfedd1b96f8880a136a2b46
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2023-07-31 09:35:51 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Deploying ALB Controller via Helm........"
+RESOURCE_GROUP='aks-west-prod'
+AKS_NAME='aks-west-prod'
 
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_NAME
 helm install alb-controller oci://mcr.microsoft.com/application-lb/charts/alb-controller \
