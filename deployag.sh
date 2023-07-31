9d3aa9b4d5e80a18c46efa0ca062828d20270de4
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2023-07-31 09:45:36 -0700

    Create deployag.sh

---
+++ deployag.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Deploying Application Gateway for Containers into Virtual Networks containing AKS Cluster"
+AKS_NAME='aks-west-prod'
+RESOURCE_GROUP='aks-west-prod'
+
+MC_RESOURCE_GROUP=$(az aks show --name $AKS_NAME --resource-group $RESOURCE_GROUP --query "nodeResourceGroup" -o tsv)
+CLUSTER_SUBNET_ID=$(az vmss list --resource-group $MC_RESOURCE_GROUP --query '[0].virtualMachineProfile.networkProfile.networkInterfaceConfigurations[0].ipConfigurations[0].subnet.id' -o tsv)
+read -d '' VNET_NAME VNET_RESOURCE_GROUP VNET_ID <<< $(az network vnet show --ids $CLUSTER_SUBNET_ID --query '[name, resourceGroup, id]' -o tsv)
